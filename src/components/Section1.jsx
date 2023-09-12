b6db6fcd9e61e8a2958e48068a8ec64e91a70f09
Sweyd Abdul Manaf <abdulsweyd@gmail.com>
2023-09-13 01:02:24 +0200

    shadow only on mobilde version

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -10,18 +10,23 @@ export default function Section1() {
         </div>
         <div className="md:flex gap-10 items-center hidden">
           <span className="text-6xl text-[#0E2624]">$430</span>
-          <button className="bg-[#30F] text-white text-3xl py-4 px-14 button-box-shadow">
+          <button className="bg-[#30F] text-white text-3xl py-4 px-14 hover:shadow-button-box-shadow ease-in duration-200">
             buy now
           </button>
         </div>
       </div>
 
-      <div className="bg-white w-fit h-fit border-2 border-black shoes1-box-shadow ">
-        <img src={Shoes1} alt="" className="py-10 md:py-20  px-2 md:px-10" />
+      <div className="bg-white w-fit h-fit border-2 border-black shadow-shoes1-mobile md:shadow-none md:hover:shadow-shoes1-mobile ease-in duration-200 relative">
+        <img src={Shoes1} alt="" className="py-10 md:py-20 px-2 md:px-10" />
+        <div className="flex w-full justify-center gap-2 -mt-8 pb-4">
+          <div className="w-4 h-4 bg-[#5642FF] border-[3px] border-black rounded-full"></div>
+          <div className="w-4 h-4 bg-[#5642FF] border-[3px] border-black rounded-full"></div>
+          <div className="w-4 h-4 bg-[#5642FF] border-[3px] border-black rounded-full"></div>
+        </div>
       </div>
 
-      <div className="md:hidden flex gap-10 items-center mt-6">
-        <span className="text-6xl text-[#0E2624]">$430</span>
+      <div className="md:hidden flex justify-between items-center mt-6">
+        <span className="text-7xl text-[#0E2624]">$430</span>
         <button className="bg-[#30F] text-white text-3xl py-4 px-14 button-box-shadow">
           buy now
         </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "shoes1-mobile": "10px 10px 0px 0px #30f",
+        "button-box-shadow": "5px 5px 0px 0px #000;",
+      },
+    },
   },
   plugins: [],
 };
